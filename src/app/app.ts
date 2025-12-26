2cc5101ad8f3632471a1d971de2d724ae1bd3916
vncebrmjo <bermejovince31@gmail.com>
2025-12-26 15:47:06 +0800

    changed app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ecommerce-website');
+  protected readonly title = signal('Vince');
 }
